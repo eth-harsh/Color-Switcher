09965f0903ddca4ec0fe824232643f2f385c2a22
Harsh <rajharsh52700@gmail.com>
2026-06-26 09:06:56 +0530

    Refactor color selection and hex input handling

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 //variables
 let counter = 0;
-document.querySelectorAll(".btn").forEach((v) => {
-  v.addEventListener("click", function () {
-    const color = this.dataset.color;
+document.querySelectorAll(".btn").forEach((b) => {
+  b.addEventListener("click", function () {
     const name = this.dataset.name;
+    const color = this.dataset.color;
+    document.querySelector(".message").textContent = "";
+    document.querySelector(".hex-input").value = "";
     document.querySelector(".color-name").textContent = name;
     document.querySelector(".color-box").style.backgroundColor = color;
     counter++;
@@ -13,23 +15,25 @@ document.querySelectorAll(".btn").forEach((v) => {
   });
 });
 
-//hex code
+//input hex
 document.querySelector(".btn-apply").addEventListener("click", function () {
-  const input = document.querySelector(".hex-input").value;
+  document.querySelector(".message").textContent = "";
+  let input = document.querySelector(".hex-input").value;
   if (input.startsWith("#") && input.length === 7) {
-    document.querySelector(".message").textContent = "";
     document.querySelector(".color-box").style.backgroundColor = input;
+    document.querySelector(".color-name").textContent = input;
     counter++;
     document.querySelector(".count").textContent = counter;
   } else {
-    document.querySelector(".message").textContent = "Invalid Hex⛔";
+    document.querySelector(".message").textContent = "Invalid hex code⛔";
   }
 });
-//reset button
+
+//reset
 document.querySelector(".btn-reset").addEventListener("click", function () {
   counter = 0;
-  document.querySelector(".count").textContent = counter;
   document.querySelector(".message").textContent = "";
-  document.querySelector(".color-box").style.backgroundColor = "#222";
   document.querySelector(".hex-input").value = "";
+  document.querySelector(".color-box").style.backgroundColor = "#222";
+  document.querySelector(".color-name").textContent = '';
 });
